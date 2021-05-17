75447bb10f27b482c209621c1791e1b36fc00668
zakeyah <mom11omar@gmail.com>
2021-05-17 08:23:56 +0300

    challeng6

--- challenges /linkedList/__test__/linkedList.test.js
+++ challenges /linkedList/__test__/linkedList.test.js
@@ -22,16 +22,33 @@ describe('test the Linked List',()=>{
     expect(linkedList.head.value).toEqual(1);
     expect(linkedList.head.next).toBeNull();
     linkedList.insert(2);
-    expect(linkedList.head.next.value).toEqual(2);
+    expect(linkedList.head.value).toEqual(2);
   });
 
   it('test  LinkedList  includes',()=>{
-    linkedList.insert(2);
-    expect(linkedList.includes(2)).toEqual(true);
+    linkedList.insert(1);
+    expect(linkedList.includes(1)).toEqual(true);
   });
 
   it('test  LinkedList  toString',()=>{
     expect(linkedList.toString()).toEqual('{ 1 } ->{ 2 } ->NULL');
+  });
+  it('test Node append ',()=>{
+    expect(linkedList.head.value).toEqual(1);
+    expect(linkedList.head.next.value).toEqual(2);
+    linkedList.append(3);
+    expect(linkedList.head.next.next.next.value).toEqual(3);
+  });
+  it('test  LinkedList  insertBefore',()=>{
+    linkedList.insertBefore(1,11);
+    expect(linkedList.head.value).toEqual(11);
+    expect(linkedList.head.next.value).toEqual(1);
+  });
+
+  it('test  LinkedList  insertAfter',()=>{
+    linkedList.insertAfter(11,5);
+    expect(linkedList.head.value).toEqual(11);
+    expect(linkedList.head.next.value).toEqual(5);
   });
 
 });
