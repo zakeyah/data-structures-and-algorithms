715407c66a9fb16a5b17a2755130ca30e37d77dc
zakeyah <mom11omar@gmail.com>
2021-06-13 20:34:09 +0300

    challenge26

---
+++ challenges /InsertionSort/README.md
@@ -0,0 +1,15 @@
+ # InsertionSort
+
+
+ ## challenge
+
+ 
+write function that take array as argument and return the array sorted by InsertionSort
+
+
+## Approach & Efficiency
+Big o
+time :o(n^2)
+space: o(1)
+
+![](/assets/challenge26.jpg)
--- challenges /fizzBuzzTree/fizz-buzz-tree.js
+++ challenges /fizzBuzzTree/fizz-buzz-tree.js
@@ -10,35 +10,36 @@ class KaryTree {
   constructor(root = null) {
     this.root = root
   }
-  fizzBuzzTree(ktree) {
-    if (!ktree.root) {
-      return 'Empty Tree';
-    }
-    this.root=ktree.root;
-    const traverse = node => {
-      // node.value-------->1,2,5,11,3,10,9,15,4,3
-      if (node.value % 3 === 0 && node.value % 5 === 0) {
-        node.value = 'FizzBuzz';
-      } else if (node.value % 5 === 0) {
-        node.value = 'Buzz';
-      } else if (node.value % 3 === 0) {
-        node.value = 'Fizz';
-      } else {
-        node.value = `${node.value}`;
-      }
-      if (node) {
-        for (let i = 0; i < node.children.length; i++) {
-          // node.children.length-------->3,1,1,3,2,1,2,3,1
-          traverse(node.children[i]);
-        }
-      }
-    };
-    traverse(this.root);
+}
+function fizzBuzzTree(ktree) {
+  if (!ktree.root) {
+    return 'Empty Tree';
   }
+  // this.root=ktree.root;
+  const traverse = node => {
+    // node.value-------->1,2,5,11,3,10,9,15,4,3
+    if (node.value % 3 === 0 && node.value % 5 === 0) {
+      node.value = 'FizzBuzz';
+    } else if (node.value % 5 === 0) {
+      node.value = 'Buzz';
+    } else if (node.value % 3 === 0) {
+      node.value = 'Fizz';
+    } else {
+      node.value = `${node.value}`;
+    }
+    if (node) {
+      for (let i = 0; i < node.children.length; i++) {
+        // node.children.length-------->3,1,1,3,2,1,2,3,1
+        traverse(node.children[i]);
+      }
+    }
+  };
+  traverse(ktree.root);
 }
 
 module.exports = {
   Node,
-  KaryTree
+  KaryTree,
+  fizzBuzzTree
 };
 
