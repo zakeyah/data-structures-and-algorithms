2a87715cee71aea9542d8c9faf6cfa3f30c0ce43
zakeyah <mom11omar@gmail.com>
2021-06-17 23:32:57 +0300

    challenge30

---
+++ challenges /hashtable/__test__/hashtable.test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const HashTable = require('../hashtable');
+let hashTable = new HashTable(1024);
+
+describe('Hash Table', () => {
+  it('should create a hashed table with length 1024', () => {
+    expect(hashTable.table.length).toEqual(1024);
+  });
+  it('should hash the key ', () => {
+    expect( hashTable.hash('key')).toEqual(107);
+  });
+  it('should add the value in the table by it is key', () => {
+    hashTable.add('key', 1234);
+    expect(hashTable.contains('key')).toBeTruthy();
+    expect(hashTable.get('key')).toEqual(1234);
+  });
+  it('should find if the value exists in the table by it is key', () => {
+    hashTable.add('key', 1234);
+    expect(hashTable.contains('key')).toBeTruthy();
+    expect(hashTable.contains('keeeeey')).toBeFalsy();
+  });
+  it('should get the value of a key if it exists in the table', () => {
+    hashTable.add('key', 1234);
+    expect(hashTable.get('key')).toEqual(1234);
+  });
+  it('should return null if the key is not in the table', () => {
+    expect(hashTable.get('not key')).toEqual(null);
+  });
+  it('should handle a collision within the hashtable', () => {
+    hashTable.add('nnaa', 1234);
+    hashTable.add('aann', 4444);
+    expect(hashTable.contains('nnaa')).toBeTruthy();
+    expect(hashTable.contains('aann')).toBeTruthy();
+    expect(hashTable.get('nnaa')).toEqual(1234);
+    expect(hashTable.get('aann')).toEqual(4444);
+  });
+});
